d56809ef8d6b2a46fd56ae884f0196eb22e4418a
Shubham Maurya <157043197+Skmaurya05550@users.noreply.github.com>
2024-10-08 14:58:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,127 @@
 
+window.onload = function(){
+
+
+
+
+const accessKey = "eVYcbFYWh1NTyxqJxKHItgzczqr3yatJhJDz3LVYMB4";
+
+
+
+const formEl = document.querySelector("form");
+
+const searchInputEl = document.getElementById("search-input");
+
+const searchResultsEl = document.querySelector(".search-results");
+
+const showMoreButtonEl = document.getElementById("show-more-button");
+
+const landingPage = document.getElementById("wrapp")
+
+
+
+let inputData = "";
+
+let page = 1;
+
+
+
+async function searchImages() {
+
+  inputData = searchInputEl.value;
+
+  if(inputData.trim()){
+
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    if (page === 1) {
+
+      searchResultsEl.innerHTML = "";
+
+    }
+
+
+
+    const results = data.results;
+
+
+
+    results.map((result) => {
+
+      const imageWrapper = document.createElement("div");
+
+      imageWrapper.classList.add("search-result");
+
+      const image = document.createElement("img");
+
+      image.src = result.urls.small;
+
+      image.alt = result.alt_description;
+
+      const imageLink = document.createElement("a");
+
+      imageLink.href = result.links.html;
+
+      imageLink.target = "_blank";
+
+      imageLink.textContent = result.alt_description;
+
+
+
+      imageWrapper.appendChild(image);
+
+      imageWrapper.appendChild(imageLink);
+
+      searchResultsEl.appendChild(imageWrapper);
+
+    });
+
+
+
+    page++;
+
+
+
+    if (page > 1) {
+
+      showMoreButtonEl.style.display = "block";
+
+    }
+
+   
+
+    if (page > 1){
+
+landingPage.style.display = "none";
+
+    }
+
+  }
+
+}
+
+
+
+formEl.addEventListener("submit", (event) => {
+
+  event.preventDefault();
+
+  page = 1;
+
+  searchImages();
+
+});
+
+
+
+showMoreButtonEl.addEventListener("click", () => {
+
+  searchImages();
+
+})};
